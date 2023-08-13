7ab0553ac1665b6e284fadbc1969229ef041b139
Hu Ziang <hza2002@foxmail.com>
2023-08-13 13:53:42 +0800

    finish part2 2.11 phonebook with db.json

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 import Filter from './component/Filter'
 import Persons from './component/Persons'
@@ -6,12 +7,21 @@ import PersonForm from './component/PersonForm'
 
 const App = () => {
   const [filter, setFilter] = useState('')
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
+
+  useEffect(() => {
+    console.log('effect')
+
+    const eventHandler = response => {
+      console.log('promise fulfilled')
+      setPersons(response.data)
+    }
+
+    const promise = axios.get('http://localhost:3001/persons')
+    promise.then(eventHandler)
+  }, [])
+
+  console.log('render', persons.length, 'notes')
 
   return (
     <div>
