34063cb7b17c2ffc8a7d21576141c0a53ba0a4cf
Hu Ziang <hza2002@foxmail.com>
2023-08-17 11:12:51 +0800

    finish part3 3.15 3.17-3.18 phonebook database

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -12,26 +12,26 @@ app.use(express.static('build'))
 app.use(morgan('tiny'))
 
 var persons = [
-  {
-    "id": 1,
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": 2,
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": 3,
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": 4,
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
-  },
+  // {
+  //   "id": 1,
+  //   "name": "Arto Hellas",
+  //   "number": "040-123456"
+  // },
+  // {
+  //   "id": 2,
+  //   "name": "Ada Lovelace",
+  //   "number": "39-44-5323523"
+  // },
+  // {
+  //   "id": 3,
+  //   "name": "Dan Abramov",
+  //   "number": "12-43-234345"
+  // },
+  // {
+  //   "id": 4,
+  //   "name": "Mary Poppendieck",
+  //   "number": "39-23-6423122"
+  // },
 ]
 
 // Main page
@@ -62,7 +62,6 @@ app.get('/api/persons', (_, response) => {
 // Get one person
 app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
-  // const person = persons.find(person => person.id === id)
   personsDB.queryOne(id)
     .then(
       person => {
@@ -130,9 +129,9 @@ app.post('/api/persons', (request, response) => {
       newPerson => {
         if (newPerson) {
           console.log(newPerson, typeof newPerson)
-          persons = persons.concat(newPerson)
           response.json(newPerson)
         } else {
+          console.log(newPerson, typeof newPerson)
           response.status(404).end()
         }
       }
--- part3/backend/modules/person.js
+++ part3/backend/modules/person.js
@@ -1,20 +1,20 @@
 require('dotenv').config()
 
-const mysql = require('mysql');
+const mysql = require('mysql')
 const databaseName = process.env.DATABASE_NAME
 const con = mysql.createConnection({
   host: process.env.HOST_NAME,
   user: process.env.USER_NAME,
   password: process.env.USER_PASSWORD,
   database: databaseName,
-});
+})
 
 // Connected
 con.connect(function(err) {
   if (err) {
     console.log('Error connecting to MySql:', err)
   } else {
-    console.log("Connected!");
+    console.log("Connected!")
   }
 })
 
@@ -23,36 +23,36 @@ const queryALL = () => {
   return new Promise((resolve, reject) => {
     con.query("SELECT * FROM persons", function(err, result) {
       if (err) {
-        console.log('Error querying all records:', err);
-        reject(err);
+        console.log('Error querying all records:', err)
+        reject(err)
       } else {
-        console.log('Query all records successful');
+        console.log('Query all records successful')
         const persons = result.map(row => ({
           id: row.id,
           name: row.name,
           number: row.number
-        }));
-        resolve(persons);
+        }))
+        resolve(persons)
       }
-    });
-  });
-};
+    })
+  })
+}
 
 // Query Record
 const queryRecord = (id) => {
   return new Promise((resolve, reject) => {
     con.query(`SELECT * FROM persons WHERE id = '${id}'`, function(err, result) {
       if (err) {
-        console.log('Error querying one record:', err);
-        reject(err);
+        console.log('Error querying one record:', err)
+        reject(err)
       } else {
-        console.log(`Query id:${id} record successful`);
+        console.log(`Query id:${id} record successful`)
         const person = result[0]
-        resolve(person);
+        resolve(person)
       }
-    });
-  });
-};
+    })
+  })
+}
 
 // Update Record
 const updateRecord = (id, number) => {
@@ -60,9 +60,9 @@ const updateRecord = (id, number) => {
     con.query(`UPDATE persons SET number = '${number}' WHERE id = '${id}'`, function(err, result) {
       if (err) {
         console.log('Error update record:', err)
-        reject(err);
+        reject(err)
       } else {
-        console.log(result, "record updated")
+        console.log(result, "Record updated")
         resolve(result)
       }
     })
@@ -74,11 +74,11 @@ const addRecord = ({ name, number }) => {
   return new Promise((resolve, reject) => {
     con.query(`INSERT INTO persons (name, number) VALUES ('${name}', '${number}')`, function(err) {
       if (err) {
-        console.log('Error add record:', err)
-        reject(err);
+        console.log('Error add record')
+        reject(err)
       } else {
-        console.log(result, "record added")
-        resolve(result)
+        console.log('Add record')
+        resolve()
       }
     })
   })
@@ -89,15 +89,15 @@ const deleteRecord = (id) => {
   return new Promise((resolve, reject) => {
     con.query(`DELETE FROM persons WHERE id = '${id}'`, function(err, result) {
       if (err) {
-        console.log('Error delete one record:', err);
-        reject(err);
+        console.log('Error delete one record:', err)
+        reject(err)
       } else {
-        console.log(`Deleted id:${id} record successful`);
-        resolve(result);
+        console.log(`Deleted id:${id} record successful`)
+        resolve(result)
       }
-    });
-  });
-};
+    })
+  })
+}
 
 module.exports = {
   queryALL,
@@ -105,4 +105,4 @@ module.exports = {
   updateRecord,
   addRecord,
   deleteRecord
-};
+}
